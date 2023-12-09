c6e42dae2d6d6e9e6b42a5868104be854aabccb2
David Duran <dadch1404@gmail.com>
2023-12-09 17:41:53 -0500

    Refactor main function to include fetchType parameter

--- examples/index.js
+++ examples/index.js
@@ -1,14 +1,14 @@
 const fetchYoutubeMetadata = require('../src/youtubeMetadata');
 
-async function main(query) {
-  const metadata = await fetchYoutubeMetadata(query);
+async function main(query, fetchType = 'fullData') {
+  const metadata = await fetchYoutubeMetadata(query, fetchType);
   console.log(metadata);
   return metadata;
 }
 
 async function runTest() {
   await main('https://www.youtube.com/watch?v=dQw4w9WgXcQ'); // this is a video (long form)
-  await main('https://youtu.be/dQw4w9WgXcQ'); // this is a video (short form)
+  await main('https://youtu.be/dQw4w9WgXcQ', 'idOnly'); // this is a video (short form), we only want the ID
   await main('https://www.youtube.com/watch?v=7AiSeatxXS4&list=PLwEkAnYJ7sRY6OLAxDBhAlUvPpAi6Am6-&pp=iAQB') // this is really a video but it's in a playlist
   await main('https://www.youtube.com/playlist?list=PLpBx-1imHuxISNflNHo0Qr4mQ8l3mqmyi') // this is a playlist
   await main('https://www.youtube.com/channel/UC_LQrLv47K0Fd3k2MB1kBRQ') // this is a channel (old format)
