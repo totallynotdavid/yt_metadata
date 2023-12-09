c1644c9a53877ff49e5bef336f0a56bd1790a24b
David Duran <dadch1404@gmail.com>
2023-12-08 23:04:57 -0500

    Refactor extractId function to handle different splitValue types

--- src/youtubeExtractor.js
+++ src/youtubeExtractor.js
@@ -2,18 +2,27 @@ const { youtubeTypes } = require('./youtubeRegex');
 
 function extractMediaId(link) {
   if (youtubeTypes.videos.test(link)) {
-    return extractId(link, youtubeTypes.videos, 5);
+    return [ extractId(link, youtubeTypes.videos, 5), 'video'];
   } else if (youtubeTypes.playlists.test(link)) {
-    return extractId(link, youtubeTypes.playlists, "list=");
+    return [ extractId(link, youtubeTypes.playlists, "list="), 'playlist'];
   } else if (youtubeTypes.channels.test(link)) {
-    return extractId(link, youtubeTypes.channels, "/channel/");
+    return [ extractId(link, youtubeTypes.channels, "/channel/"), 'channel'];
   }
   return null;
 }
 
 function extractId(link, regex, splitValue) {
   const match = link.match(regex);
-  return match ? match[0].split(splitValue)[1] : null;
+  if (!match) return null;
+
+  // Different handling based on the type of splitValue
+  if (typeof splitValue === 'number') {
+    // If it's a number, it's used as an index for the regex match groups
+    return match[splitValue];
+  } else {
+    // If it's a string, it's used to split the matched string
+    return match[0].split(splitValue)[1];
+  }
 }
 
 module.exports = {
