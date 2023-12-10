205e9aa2fea99dbe3a2d90abe24b00b0c08f69ed
David Duran <dadch1404@gmail.com>
2023-12-09 21:09:36 -0500

    Add query examples

--- examples/index.js
+++ examples/index.js
@@ -7,12 +7,17 @@ async function main(query, fetchType = 'fullData') {
 }
 
 async function runTest() {
-  await main('https://www.youtube.com/watch?v=dQw4w9WgXcQ'); // this is a video (long form)
-  await main('https://youtu.be/dQw4w9WgXcQ', 'idOnly'); // this is a video (short form), we only want the ID
+  // Case 1: URLs
+  await main('https://www.youtube.com/watch?v=dQw4w9WgXcQ');  // this is a video (long form)
+  await main('https://youtu.be/dQw4w9WgXcQ', 'idOnly');       // this is a video (short form), we only want the ID
   await main('https://www.youtube.com/watch?v=7AiSeatxXS4&list=PLwEkAnYJ7sRY6OLAxDBhAlUvPpAi6Am6-&pp=iAQB') // this is really a video but it's in a playlist
   await main('https://www.youtube.com/playlist?list=PLpBx-1imHuxISNflNHo0Qr4mQ8l3mqmyi') // this is a playlist
-  await main('https://www.youtube.com/channel/UC_LQrLv47K0Fd3k2MB1kBRQ') // this is a channel (old format)
-  await main('https://www.youtube.com/@CulturaSiglo21') // this is a channel (new format)
+  await main('https://www.youtube.com/channel/UC_LQrLv47K0Fd3k2MB1kBRQ')  // this is a channel (old format)
+  await main('https://www.youtube.com/@CulturaSiglo21')                   // this is a channel (new format)
+
+  // Case 2: Queries 
+  await main('Oversimplified', 'idOnly')
+  await main('omg new jeans', 'idOnly')
 }
 
 runTest();
